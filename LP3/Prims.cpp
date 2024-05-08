bb91a25ad324c2a455bea5727ada1df5f426e910
Ravi2320 <rbkadam2320@gmail.com>
2024-05-08 20:46:52 +0530

    Changed prims code

--- LP3/Prims.cpp
+++ LP3/Prims.cpp
@@ -2,64 +2,71 @@
 
 using namespace std;
 
-class Solution
+vector<pair<int, pair<int, int>>> prims(int v, vector<vector<int>> adj[])
 {
-	public:
-		int SpanningTree(int v, vector<vector<int>> adj[])
-		{
-			priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
 
-			vector<int> vis(v, 0);
-			pq.push({0, 0});
-			int sum = 0;
-			while(!pq.empty())
-			{
-				auto it = pq.top();
-				pq.pop();
-				int node = it.second;
-				int wt = it.first;
+    vector<int> visited(v, 0);
+    pq.push({0, {0, -1}});
+    vector<pair<int, pair<int, int>>> mstEdges; // Vector to store edges of MST
+    int sum = 0;
+    while(!pq.empty()){
 
-				if(vis[node] == 1)
-					continue;
+        auto it = pq.top();
+        pq.pop();
 
-				vis[node] = 1;
-				sum += wt;
+        int Wt = it.first;
+        int node = it.second.first;
+        int parent = it.second.second;
 
-				for(auto it : adj[node])
-				{
-					int adjNode = it[0];
-					int edWt = it[1];
-					
-					if(!vis[adjNode])
-						pq.push({edWt, adjNode});
-				}
-			}
-			return sum;
-		}
-};
+        if(visited[node] == 1)
+            continue;
+
+        visited[node] = 1;
+        sum += Wt;
+        
+        if(parent != -1) {
+            mstEdges.push_back({Wt, {parent, node}});
+        }
+        
+        for(auto it : adj[node])
+        {
+            int adjNode = it[0];
+            int adjwt = it[1];
+
+            if(!visited[adjNode])
+                pq.push({adjwt, {adjNode, node}});
+            
+        }
+    }
+    cout << "Total Sum of MST Edges : " << sum;
+    return mstEdges;
+}
 
 int main()
 {
-	Solution obj;
-	int v = 5;
-	vector<vector<int>> edges = {{0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}};
-	vector<vector<int>> adj[v];
+    int v = 5;
+    vector<vector<int>> adj[v];
 
-	for(auto it : edges)
-	{
-		vector<int> tmp(2);
-		tmp[0] = it[1];
-		tmp[1] = it[2];
-		adj[it[0]].push_back(tmp);
-
-		tmp[0] = it[0];
-		tmp[1] = it[2];
-		adj[it[1]].push_back(tmp);
-	}
-
-	int sum = obj.SpanningTree(v, adj);
-	
-	cout << "The sum of all MST edge weights are : " << sum << endl;
-
-	return 0;
+    adj[0].push_back({1, 2});
+    adj[0].push_back({2, 1});
+    adj[1].push_back({0, 2});
+    adj[1].push_back({2, 1});
+    adj[2].push_back({0, 1});
+    adj[2].push_back({1, 1});
+    adj[2].push_back({3, 2});
+    adj[2].push_back({4, 2});
+    adj[3].push_back({2, 2});
+    adj[3].push_back({4, 1});
+    adj[4].push_back({2, 2});
+    adj[4].push_back({3, 1});
+    
+    vector<pair<int, pair<int, int>>> mstEdges = prims(v, adj);
+    
+    cout << "\nEdge \t Weight" << endl;
+    for(auto edge : mstEdges) {
+        cout << edge.second.first << " - " << edge.second.second << "\t   " << edge.first << endl;
+    }
+    
+    return 0;
 }
